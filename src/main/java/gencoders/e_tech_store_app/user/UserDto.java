1e8c31d3fd59556aedc3bb28544b9ab27736076f
Yvette IRADUKUNDA <iradyvette98@gmail.com>
2025-06-20 10:29:04 +0200

    Adding changes

--- src/main/java/gencoders/e_tech_store_app/user/UserDto.java
+++ src/main/java/gencoders/e_tech_store_app/user/UserDto.java
@@ -1,15 +1,14 @@
 package gencoders.e_tech_store_app.user;
 
 
-import lombok.AllArgsConstructor;
+
 import lombok.Getter;
-import lombok.NoArgsConstructor;
+
 import lombok.Setter;
 
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
+
 public class UserDto {
 
 }
